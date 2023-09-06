19ea2f98b2c0691d4e54c79cd7cefb11a7d4448b
Jordan Esquivel <jordan11.tauro@gmail.com>
2023-09-05 19:34:16 -0500

    Añade rutas para obtener lista de usuarios y usuario por ID

---
+++ routes/users.js
@@ -0,0 +1,45 @@
+const express = require('express');
+
+const router = express.Router();
+const fs = require('fs');
+const path = require('path');
+
+// Ruta para obtener la lista completa de usuarios
+router.get('/', (req, res) => {
+  const usersPath = path.join(__dirname, '..', 'data', 'users.json');
+  fs.readFile(usersPath, 'utf8', (err, data) => {
+    try {
+      if (err) {
+        throw new Error('Error al leer el archivo');
+      }
+      res.json(JSON.parse(data));
+    } catch (error) {
+      res.status(500).json({ message: '¡Algo salió mal!' });
+    }
+  });
+});
+
+// Ruta para obtener un usuario por ID
+router.get('/:id', (req, res) => {
+  const usersPath = path.join(__dirname, '..', 'data', 'users.json');
+  fs.readFile(usersPath, 'utf8', (err, data) => {
+    try {
+      if (err) {
+        throw new Error('Error al leer el archivo');
+      }
+
+      const users = JSON.parse(data);
+      const user = users.find((u) => u._id === req.params.id);
+
+      if (!user) {
+        res.status(404).json({ message: 'ID de usuario no encontrado' });
+      }
+
+      res.json(user);
+    } catch (error) {
+      res.status(500).json({ message: '¡Algo salió mal!' });
+    }
+  });
+});
+
+module.exports = router;
