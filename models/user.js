e1b51f6cee03e521b0568836b2a06703bfd1d124
Jordan Esquivel <jordan11.tauro@gmail.com>
2023-09-15 10:11:45 -0500

    Agregar notacion abreviada

--- models/user.js
+++ models/user.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 
 // Expresión regular para validar URLs
-const urlRegex =
-  /^(http|https):\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)$/;
+const urlRegex = /^(http|https):\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)$/;
 
 // Esquema del usuario
 const UserSchema = new mongoose.Schema({
@@ -22,7 +21,7 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: true,
     validate: {
-      validator: function (v) {
+      validator(v) {
         return urlRegex.test(v);
       },
       message: (props) => `${props.value} no es una URL válida!`,
