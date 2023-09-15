807e7ad880c75d889519182f64b5063304d16e0a
Jordan Esquivel <jordan11.tauro@gmail.com>
2023-09-15 09:40:07 -0500

    Configuración inicial base de datos

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 /* eslint-disable import/no-dynamic-require */
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = 3000;
@@ -22,4 +23,18 @@ app.use((req, res) => {
   res.status(500).send('¡Algo salió mal!');
 });
 
-app.listen(PORT, () => {});
+mongoose.connect('mongodb://localhost:27017/aroundb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'Error de conexión a MongoDB:'));
+db.once('open', () => {
+  console.log('Conectado a MongoDB');
+
+  app.listen(PORT, () => {});
+});
