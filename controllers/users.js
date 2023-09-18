bcd5374227de9ba2b3eb3552874b4e096626a353
Jordan Esquivel <jordan11.tauro@gmail.com>
2023-09-17 19:03:33 -0500

    Agregar updateUserProfile y updateUserAvatar

--- controllers/users.js
+++ controllers/users.js
@@ -40,8 +40,61 @@ const createUser = async (req, res) => {
   }
 };
 
+const updateUserProfile = async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ['name', 'about'];
+  const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
+
+  if (!isValidOperation) {
+    return res.status(400).send({ message: 'Actualizaciones no válidas!' });
+  }
+
+  try {
+    const user = await User.findById(req.user._id).orFail(new Error('Usuario no encontrado'));
+
+    updates.forEach((update) => {
+      user[update] = req.body[update];
+    });
+
+    await user.save();
+    return res.send(user);
+  } catch (err) {
+    if (err.message === 'Usuario no encontrado') {
+      return res.status(404).send({ message: err.message });
+    }
+    return res.status(400).send({ message: 'Error al actualizar el usuario' });
+  }
+};
+
+const updateUserAvatar = async (req, res) => {
+  try {
+    if (!req.body.avatar) {
+      return res
+        .status(400)
+        .send({ message: 'El campo avatar es requerido para esta operación.' });
+    }
+
+    const user = await User.findById(req.user._id).orFail(new Error('Usuario no encontrado'));
+
+    user.avatar = req.body.avatar;
+    await user.save();
+
+    return res.send(user);
+  } catch (err) {
+    if (err.message === 'Usuario no encontrado') {
+      return res.status(404).send({ message: err.message });
+    }
+    if (err.name === 'ValidationError') {
+      return res.status(400).send({ message: err.message });
+    }
+    return res.status(400).send({ message: 'Error al actualizar el avatar' });
+  }
+};
+
 module.exports = {
   getUsers,
   getUserById,
   createUser,
+  updateUserProfile,
+  updateUserAvatar,
 };
