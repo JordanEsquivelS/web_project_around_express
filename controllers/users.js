c10b155657bb144288f951fcab5e6d9cadf4ac0d
Jordan Esquivel <jordan11.tauro@gmail.com>
2023-09-19 12:19:15 -0500

    Agregar uso middleware centralizado y correcion mensajes de error

--- controllers/users.js
+++ controllers/users.js
@@ -4,56 +4,65 @@ const path = require('path');
 
 const User = require(path.join(__dirname, '..', 'models', 'user'));
 
-const getUsers = async (req, res) => {
+const getUsers = async (req, res, next) => {
   try {
     const users = await User.find({});
     res.status(200).send(users);
   } catch (err) {
-    res
-      .status(500)
-      .send({ message: 'No se pudo obtener la lista de usuarios' });
+    err.message = 'No se pudo obtener la lista de usuarios';
+    next(err);
   }
 };
 
-const getUserById = async (req, res) => {
+const getUserById = async (req, res, next) => {
   const userId = req.params._id;
 
   if (!mongoose.Types.ObjectId.isValid(userId)) {
-    return res.status(404).send({ message: 'ID de usuario no válido' });
+    const error = new Error('ID de usuario no válido');
+    error.status = 404;
+    return next(error);
   }
 
   try {
     const user = await User.findById(userId).orFail(() => {
-      const error = new Error('No se ha encontrado ningún usuario con ese ID');
-      error.statusCode = 404;
+      const error = new Error('ID de Usuario no encontrado');
+      error.status = 404;
       throw error;
     });
 
     return res.status(200).send(user);
   } catch (err) {
-    const status = err.statusCode || 500;
-    const message = err.message || 'Error al obtener el usuario';
-    return res.status(status).send({ message });
+    err.message = err.message || 'Error al obtener el usuario';
+    return next(err);
   }
 };
 
-const createUser = async (req, res) => {
+const createUser = async (req, res, next) => {
   try {
     const user = new User(req.body);
     await user.save();
     res.status(201).send(user);
   } catch (err) {
-    res.status(400).send({ message: 'Error al crear el usuario' });
+    err.status = 400;
+    if (err.name === 'ValidationError' && err.errors.avatar) {
+      err.message = 'No se creó el usuario debido a una URL no válida';
+    } else {
+      err.message = 'Error al crear el usuario';
+    }
+
+    next(err);
   }
 };
 
-const updateUserProfile = async (req, res) => {
+const updateUserProfile = async (req, res, next) => {
   const updates = Object.keys(req.body);
   const allowedUpdates = ['name', 'about'];
   const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
 
   if (!isValidOperation) {
-    return res.status(400).send({ message: 'Actualizaciones no válidas!' });
+    const error = new Error('Actualizaciones no válidas!');
+    error.status = 400;
+    return next(error);
   }
 
   try {
@@ -67,18 +76,19 @@ const updateUserProfile = async (req, res) => {
     return res.send(user);
   } catch (err) {
     if (err.message === 'Usuario no encontrado') {
-      return res.status(404).send({ message: err.message });
+      err.status = 404;
+    } else {
+      err.status = 400;
+      err.message = 'Error al actualizar el usuario';
     }
-    return res.status(400).send({ message: 'Error al actualizar el usuario' });
+    return next(err);
   }
 };
 
-const updateUserAvatar = async (req, res) => {
+const updateUserAvatar = async (req, res, next) => {
   try {
     if (!req.body.avatar) {
-      return res
-        .status(400)
-        .send({ message: 'El campo avatar es requerido para esta operación.' });
+      return res.status(400).send({ message: 'El campo avatar es requerido para esta operación.' });
     }
 
     const user = await User.findById(req.user._id).orFail(new Error('Usuario no encontrado'));
@@ -89,12 +99,14 @@ const updateUserAvatar = async (req, res) => {
     return res.send(user);
   } catch (err) {
     if (err.message === 'Usuario no encontrado') {
-      return res.status(404).send({ message: err.message });
+      err.status = 404;
+    } else if (err.name === 'ValidationError') {
+      err.status = 400;
+    } else {
+      err.status = 400;
+      err.message = 'Error al actualizar el avatar';
     }
-    if (err.name === 'ValidationError') {
-      return res.status(400).send({ message: err.message });
-    }
-    return res.status(400).send({ message: 'Error al actualizar el avatar' });
+    return next(err);
   }
 };
 
